f901cab962d60cad2a3a4c34479dc7ae07eae3f4
Phillipe17Macedo <ph.stos.udia@gmail.com>
2024-08-28 12:11:51 -0300

    main0.4

--- my_flask_app/app.py
+++ my_flask_app/app.py
@@ -9,10 +9,6 @@ app = Flask(__name__)
 model_path = os.path.join(os.path.dirname(__file__), 'pmml_arqv_rendom_forest_learner.pmml')
 model = Model.load(model_path)
 
-def desnormalize_value(norm_value, orig_min, orig_max, norm_min, norm_max):
-    """Desnormaliza o valor usando a fórmula inversa da LinearNorm."""
-    return orig_min + ((norm_value - norm_min) * (orig_max - orig_min)) / (norm_max - norm_min)
-
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -38,37 +34,24 @@ def predict():
         # Preparar os dados para o modelo
         data = pd.DataFrame([input_data])
 
+        # Debug: Exibir o DataFrame preparado para o modelo
+        print("DataFrame para predição:", data)
+
         # Fazer a previsão com o modelo carregado
         result = model.predict(data)
 
         # Debug: Exibir todos os resultados retornados pelo modelo
         print("Resultado completo do modelo:", result)
 
-        # Desnormalizar os resultados para torná-los interpretáveis
-        desnormalized_year = desnormalize_value(
-            result['year*'][0],
-            orig_min=2010.0, orig_max=2035.0,
-            norm_min=-383.1185786060764, norm_max=-382.9288625763933
-        )
-        desnormalized_value = desnormalize_value(
-            result['value*'][0],
-            orig_min=1.29999998534913E-5, orig_max=4.4E8,
-            norm_min=-0.07227711653569541, norm_max=-0.07227700451014178
-        )
+        # Obter a predição (ajuste conforme necessário)
+        prediction = result.iloc[0].to_dict()
 
-        # Debug: Exibir os valores desnormalizados
-        print("Year desnormalizado:", desnormalized_year)
-        print("Value desnormalizado:", desnormalized_value)
-
-        # Passar os valores desnormalizados para o template HTML
+        # Passar os valores para o template HTML
         output = {
             'region': input_data['region'],
             'year': input_data['year'],
             'value': input_data['value'],
-            'prediction': {
-                'Desnormalized Year': desnormalized_year,
-                'Desnormalized Value': desnormalized_value
-            }
+            'prediction': prediction  # Ajuste para a classe prevista
         }
 
         return render_template('result.html', result=output)
